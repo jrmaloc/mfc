593bd4fc9e6dc4a33d17e4f44da740bc0b30c1c1
jrmaloc <jhonrobertaloc8@gmail.com>
2024-02-15 10:31:54 +0800

    ssssss

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -13,6 +13,6 @@ class WebhookController extends Controller
         $payload = $request->all();
 
         // Return a response (optional)
-        return response()->json(['message' => 'Webhook received successfully']);
+        return response()->json(['message' => 'Webhook received successfully', $payload]);
     }
 }
