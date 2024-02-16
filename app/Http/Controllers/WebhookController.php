58d62f83d384ddede944de816681520bc50eff64
jrmaloc <jhonrobertaloc8@gmail.com>
2024-02-16 09:20:33 +0800

    webbhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Aceraven777\PayMaya\API\Checkout;
+use Aceraven777\PayMaya\PayMayaSDK;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -9,12 +11,35 @@ class WebhookController extends Controller
 {
     public function handle(Request $request)
     {
-        // Parse and process the webhook payload
-        $payload = $request->all();
+        // // Parse and process the webhook payload
+        // $payload = $request->all();
 
-        // Return a response (optional)
-        return response()->json(['message' => 'Webhook received successfully', $payload]);
+        // // Return a response (optional)
+        // return response()->json(['message' => 'Webhook received successfully', $payload]);
 
         // return redirect(route('checkout.success'));
+
+        PayMayaSDK::getInstance()->initCheckout(
+            config('paymaya.public_key'),
+            config('paymaya.secret_key'),
+            (app()->environment('production') ? 'PRODUCTION' : 'SANDBOX')
+        );
+
+        $transaction_id = $request->get('id');
+        if (!$transaction_id) {
+            return ['status' => false, 'message' => 'Transaction Id Missing'];
+        }
+
+        $itemCheckout = new Checkout();
+        $itemCheckout->id = $transaction_id;
+
+        $checkout = $itemCheckout->retrieve();
+
+        if ($checkout === false) {
+            $error = $itemCheckout::getError();
+            return redirect()->back()->withErrors(['message' => $error['message']]);
+        }
+
+        return response()->json(['success', $checkout]);
     }
 }
